2af8f47ab2bd734df353393a3d591fead300425f
jatin-9096 <jatinsharmasamchana@gmail.com>
2026-06-25 08:22:10 +0530

    Initialize environment variables for API and repo
    Added environment variable configurations for API keys and repository details.

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+# ... (बाकी import कोड)
+
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
+GITHUB_TOKEN = os.environ.get("MY_PERSONAL_TOKEN")
+GITHUB_USERNAME = os.environ.get("GITHUB_USERNAME")
+GITHUB_REPO = os.environ.get("GITHUB_REPO")
+FILE_PATH = "index.html"
+# ... (बाकी फंक्शन कोड)
+
